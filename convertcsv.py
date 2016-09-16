174827c3c7d68d92f797e81cd343f695225e03c5
fiiidget <s.hackney@pitt.edu>
2016-09-16 12:33:59 -0400

    placeholder
    tip when this is needed

--- convertcsv.py
+++ convertcsv.py
@@ -1,2 +1,10 @@
 from xmlutils import xml2csv
 import csv
+import sys
+import glob
+
+inputs = "myfile.xml"
+output = "myfile.csv"
+
+converter = xml2csv(inputs, output)
+converter.convert(tag="item")
